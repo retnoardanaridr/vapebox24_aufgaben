bc2b481421cff9926320fcac2d95ca9e24477d41
Retno Ardanari <retnoardanari1728@gmail.com>
2026-02-15 12:22:11 +0700

    eslint

--- src/components/scrolltotop.jsx
+++ src/components/scrolltotop.jsx
@@ -1,5 +1,4 @@
-// eslint-disable-next-line no-unused-vars
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 export default function ScrollToTop() {
